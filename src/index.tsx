782e8a1d0a31fd470c06b41125c24ebada5b760c
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-02-04 19:26:29 -0700

    now its a cool gradient with my name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ReactDefaultApp from './ReactDefaultApp';
 import reportWebVitals from './reportWebVitals';
 import HomePage from "./HomePage";
 
