de20cadccede6a0d6504de11ba4861bf6c81baa3
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-09-27 11:27:18 -0600

    fix apparent security issue with links

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -13,13 +13,13 @@ function HomePage() {
             {/*    <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank">YouTube</a>*/}
             {/*</div>*/}
             <div className="links" id="socialLinks">
-                <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank"><i
+                <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank" rel="noreferrer"><i
                     className="fa fa-youtube"/></a>
-                <a className="link" href="https://instagram.com/dallin.from.earth" target="_blank"><i
+                <a className="link" href="https://instagram.com/dallin.from.earth" target="_blank" rel="noreferrer"><i
                     className="fa fa-instagram"/></a>
-                <a className="link" href="https://github.com/DallinFromEarth" target="_blank"><i
+                <a className="link" href="https://github.com/DallinFromEarth" target="_blank" rel="noreferrer"><i
                     className="fa fa-github-square"/></a>
-                <a className="link" href="https://www.linkedin.com/in/dallin-webecke/" target="_blank"><i
+                <a className="link" href="https://www.linkedin.com/in/dallin-webecke/" target="_blank" rel="noreferrer"><i
                     className="fa fa-linkedin-square"/></a>
             </div>
         </div>
