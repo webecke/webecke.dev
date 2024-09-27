7c431f4951b173b3627b46c390915c3695eb2bb7
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-09-27 11:23:57 -0600

    add social links

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -8,6 +8,20 @@ function HomePage() {
                 <p className={"headerName smallHeaderName"}>DALLIN</p>
                 <p className={"headerName bigHeaderName"}>WEBECKE</p>
             </div>
+            {/*<div className="links" id="mainLinks">*/}
+            {/*    <a className="link" href="https://isitchristmastime.com" target="_blank">Christmas</a>*/}
+            {/*    <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank">YouTube</a>*/}
+            {/*</div>*/}
+            <div className="links" id="socialLinks">
+                <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank"><i
+                    className="fa fa-youtube"/></a>
+                <a className="link" href="https://instagram.com/dallin.from.earth" target="_blank"><i
+                    className="fa fa-instagram"/></a>
+                <a className="link" href="https://github.com/DallinFromEarth" target="_blank"><i
+                    className="fa fa-github-square"/></a>
+                <a className="link" href="https://www.linkedin.com/in/dallin-webecke/" target="_blank"><i
+                    className="fa fa-linkedin-square"/></a>
+            </div>
         </div>
     );
 }
