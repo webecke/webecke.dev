7131a4bbc40bdebc0857ce33defb021d96f1fcb0
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-10-02 08:11:05 -0600

    fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from "./HomePage";
 import ForestBlue from "./ForestBlue";
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
