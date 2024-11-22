e94c0b989bb6b00cdc7de36fd0934be3ad3e1088
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-11-21 22:41:39 -0700

    rebrand

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import './index.css';
+import { ReactComponent as WebeckeLogo } from './webeckedev.svg';
 
 function HomePage() {
     return (
         <div className="App">
-            <div className="nameHeader">
-                <p className={"headerName smallHeaderName"}>DALLIN</p>
-                <p className={"headerName bigHeaderName"}>WEBECKE</p>
+            <div>
+                <WebeckeLogo className="logo-svg"/>
             </div>
-            {/*<div className="links" id="mainLinks">*/}
-            {/*    <a className="link" href="https://isitchristmastime.com" target="_blank">Christmas</a>*/}
-            {/*    <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank">YouTube</a>*/}
-            {/*</div>*/}
+
             <div className="links" id="socialLinks">
                 <a className="link" href="https://youtube.com/@DallinWebecke" target="_blank" rel="noreferrer"><i
                     className="fa fa-youtube"/></a>
@@ -22,6 +19,8 @@ function HomePage() {
                 <a className="link" href="https://www.linkedin.com/in/dallin-webecke/" target="_blank" rel="noreferrer"><i
                     className="fa fa-linkedin-square"/></a>
             </div>
+            <p className={"headerName"}>Dallin Webecke</p>
+            <p className={"headerName"}>Developer • Videographer • Disciple</p>
         </div>
     );
 }
